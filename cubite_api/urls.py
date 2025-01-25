12a34110eaae43dfdeae045a6a34e79a85961209
Amir Tadrisi <amirtds@gmail.com>
2025-01-25 08:36:44 -0500

    feat: add outline endpoint

--- cubite_api/urls.py
+++ cubite_api/urls.py
@@ -6,4 +6,5 @@ app_name = 'cubite_api'
 urlpatterns = [
     path('enrollment', views.Enrollments.as_view(), name='enrollment'),
     path('get_user_info', views.GetUserInfo.as_view(), name='get_user_info'),
+    path('get_course_outline', views.GetCourseOutline.as_view(), name='get_course_outline'),
 ]
